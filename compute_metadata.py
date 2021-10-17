a25de58c5e553fd8722cd5ee8727ed6357911ffd
Stephan Klinger <staeff@arcor.de>
2021-10-17 23:21:30 +0200

    First version of Django fixture

--- compute_metadata.py
+++ compute_metadata.py
@@ -1,7 +1,32 @@
 from pathlib import Path
+import json
 
-cwd = Path('.')
-xml_files = list(cwd.glob('xml/*.xml'))
+"""
+[{
+    "model": "vorwaerts.newspaperpage",
+    "pk": 2,
+    "fields": {
+        "image_name": "vw-1891-12-20-298-012.jpg"
+    }
+}, ...]
+"""
 
-for xml_file in xml_files:
-     print(xml_file.stem)
+
+
+if __name__ == '__main__':
+    cwd = Path('.')
+    xml_files = list(cwd.glob('xml/*.xml'))
+
+    fixture = []
+
+    for i, xml_file in enumerate(xml_files):
+        page = dict(
+                model="vorwaerts.newspaperpage",
+                fields={}
+                )
+        page["pk"] = i
+        page["fields"]["image_name"] = f"{xml_file.stem}.jpg"
+        fixture.append(page)
+
+    with open("pages.json", "w") as outfile:
+        json.dump(fixture, outfile)
