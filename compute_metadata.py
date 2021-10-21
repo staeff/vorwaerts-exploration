92a0dfcb1494e095613b2e3c51ebd005ec071d35
Stephan Klinger <staeff@arcor.de>
2021-10-21 15:36:39 +0200

    Extract publish date etc from filenames

--- compute_metadata.py
+++ compute_metadata.py
@@ -9,9 +9,15 @@ import json
         "image_name": "vw-1891-12-20-298-012.jpg"
     }
 }, ...]
+
+Sonntagsausgaben des „Vorwärts“ umfassen.
+Die Dateibenennung setzt sich zusammen aus dem Kürzel für die Zeitung (vw),
+dem Datum, der Ausgabennummer sowie der Seitenzahl:
+vw-jjjj-mm-tt-Ausgabennummer-Seitenzahl.
+
+Die Ausgabennummer kann ein- oder zweistellig sein.
+Die Seitenzahl ist dreistellig.
 """
-
-
 
 if __name__ == '__main__':
     cwd = Path('.')
@@ -20,12 +26,18 @@ if __name__ == '__main__':
     fixture = []
 
     for i, xml_file in enumerate(xml_files):
+        file_id = xml_file.stem
+        _, year, month, day, issue_number, page_number = file_id.split('-')
+        fields={}
         page = dict(
-                model="vorwaerts.newspaperpage",
-                fields={}
-                )
-        page["pk"] = i
-        page["fields"]["image_name"] = f"{xml_file.stem}.jpg"
+            model="vorwaerts.newspaperpage",
+            pk = i
+            )
+        fields["image_name"] = f"{file_id}.jpg"
+        fields["publish_date"] = f"{year}-{month}-{day}"
+        fields["issue_number"] = int(issue_number)
+        fields["page_number"] = int(page_number)
+        page["fields"] = fields
         fixture.append(page)
 
     with open("pages.json", "w") as outfile:
